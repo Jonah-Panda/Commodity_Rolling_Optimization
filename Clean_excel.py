7edc3057b45b3842b9ee5a17fbe0d1fb4be70f9c
Jonah Panda <jonah.pandarinath@ucalgary.ca>
2023-10-08 12:17:28 -0600

    created data folders

--- Clean_excel.py
+++ Clean_excel.py
@@ -3,11 +3,24 @@ import os
 
 cwd = os.getcwd()
 
-file_name = "{}\FNCE 449 - Final Project.xlsx".format(cwd)
-sheet_name = "Building Table"
-df = pd.read_excel(io=file_name, sheet_name=sheet_name, header=None, skiprows=27)
-df = df[[0, 1]]
-df.columns = ["Commodity", "Code"]
-df = df[0:28]
-df_comdty = df
+def get_commodity_codes_df():
+    file_name = "{}\FNCE 449 - Final Project.xlsx".format(cwd)
+    sheet_name = "Building Table"
+    df = pd.read_excel(io=file_name, sheet_name=sheet_name, header=None, skiprows=27)
+    df = df[[0, 1]]
+    df.columns = ["Commodity", "Code"]
+    df = df[0:27]
+    return df
+
+def create_folders_for_comdty_data(df_comdty):
+    for index, row in df_comdty.iterrows():
+        comdty_code = row['Code']
+        if comdty_code not in os.listdir('{}\Data'.format(cwd)):
+            os.mkdir("{}\Data\{}".format(cwd, comdty_code))
+    return
+
+
+df_comdty = get_commodity_codes_df()
+create_folders_for_comdty_data(df_comdty)
+
 print(df_comdty)
