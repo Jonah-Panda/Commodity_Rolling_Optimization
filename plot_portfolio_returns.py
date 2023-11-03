226578f1ddbcc579054724d9fb67b8322800c21d
Jonah Panda <jonah.pandarinath@ucalgary.ca>
2023-11-03 03:31:08 -0600

    finalize plots

--- plot_portfolio_returns.py
+++ plot_portfolio_returns.py
@@ -13,29 +13,37 @@ df = pd.read_csv('Returns.csv', index_col=0)
 df['Date'] = pd.to_datetime(df['Date'])
 df = df[df['Date'] >= START_DATE]
 print(df)
+# var_df = np.sqrt(df.loc[:, "TW_sam":"Comb_honey"].var()) * 100
+# var_df.to_csv('{}\Images_plots\{}'.format(cwd, "sd_results.csv"))
+# exit()
 
 x = df['Date'].to_numpy()
 strategies = df.columns.tolist()[1:-1]
 
-plt.rcParams["figure.figsize"] = (14, 8)
+plt.rcParams["figure.figsize"] = (8, 6)
 
 for strat in strategies:
     if strat[0] == "W":
         continue
-    elif strat[0] == "" or strat[-1] == "y":
+    elif strat[0] == "C" or strat[-1] == "":
         ret = df['{}'.format(strat)].to_numpy()
         ret[0] = ret[0] * STARTING_CAPITAL
         y = np.cumprod(ret)
         plt.plot(x, y, label='{}'.format(strat))
 
 plt.legend(loc='lower right')
-plt.show()
+plt.title('Combined Minimum Variance $\delta$ Weighted (all methods)')
+plt.ylabel('Return vs $100')
+# plt.savefig('{}\Images_plots\{}'.format(cwd, "comb_plot"))
+# plt.show()
 
 plt.close()
-plt.rcParams["figure.figsize"] = (8, 5)
+plt.rcParams["figure.figsize"] = (12, 5)
 plt.plot(x, df['delta_honey'].to_numpy(), label='$\delta$ honey')
 plt.ylabel('$\delta$')
 plt.title('$\delta$ Over Time')
 plt.legend(loc='best')
 plt.savefig('{}\Images_plots\{}'.format(cwd, "delta_honey"))
 plt.show()
+print(max(df['delta_honey']))
+print(min(df['delta_honey']))
